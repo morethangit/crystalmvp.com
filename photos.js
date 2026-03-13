777c77b838266a672c5cbe60d3dff2f9aabb03bc
morethangit <nolan@Nolans-MacBook-Air-2.local>
2026-03-13 16:01:35 -0400

    photo changes

---
+++ photos.js
@@ -0,0 +1,18 @@
+// Centralized image URLs for CrystalMVP template
+// Update these URLs to change images site-wide
+
+const PHOTOS = {
+  heroSlides: [
+    'https://i.imgur.com/MDgbIol.jpeg', // Slide 1
+    'https://i.imgur.com/qdsJsrw.jpeg', // Slide 2
+    'https://i.imgur.com/I2tO19L.jpeg', // Slide 3
+    'https://i.imgur.com/e7foQ2d.jpeg', // Slide 4
+    'https://i.imgur.com/XPCHYw4.jpeg', // Slide 5
+  ],
+  destinations: {
+    disney: 'https://i.imgur.com/3ST9ps3.jpeg',
+    caribbean: 'https://i.imgur.com/PyF1Q7m.jpeg',
+    maldives: 'https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?w=800&q=80',
+    europe: 'https://images.unsplash.com/photo-1499678329028-101435549a4e?w=800&q=80',
+  }
+};
